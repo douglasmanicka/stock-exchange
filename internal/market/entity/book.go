e0e3c612b44f6cca5c14a169a25c40a4d2c52391
douglasmanicka <danidev2020@gmail.com>
2023-06-27 21:55:16 -0300

    add orders_queue/book

---
+++ internal/market/entity/book.go
@@ -0,0 +1,33 @@
+package entity
+
+import (
+	"container/heap"
+	"sync"
+)
+
+type Book struct {
+	Order         []*Order
+	Transactions  []*Transaction
+	OrdersChan    chan *Order //input from Kafka
+	OrdersChanOut chan *Order
+	Wg            *sync.WaitGroup
+}
+
+func NewBook(orderChan chan *Order, orderChanOut chan *Order, wg *sync.WaitGroup) *Book {
+	return &Book{
+		Order:         []*Order{},
+		Transactions:  []*Transaction{},
+		OrdersChan:    orderChan,
+		OrdersChanOut: orderChanOut,
+		Wg:            wg,
+	}
+}
+
+func (b *Book) Trade() {
+	buyOrders := NewOrderQueue()
+	sellOrders := NewOrderQueue()
+
+	heap.Init(buyOrders)
+	heap.Init(sellOrders)
+
+}
